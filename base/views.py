4605c3d0020a869b1ec17e7a62853610a91f5670
imanirak <karinadgarces@gmail.com>
2022-07-30 14:41:23 -0400

    models and form.py created

---
+++ base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
