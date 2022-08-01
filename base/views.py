dc8a3a7ff3d3392fb3af12b21953a98a3b105e10
imanirak <karinadgarces@gmail.com>
2022-07-31 22:17:39 -0400

    created topic and activity components

--- base/views.py
+++ base/views.py
@@ -76,16 +76,16 @@ def home(request):
     ) 
     
     
-    
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'topics': topics, 'rooms': rooms, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'topics': topics, 'rooms': rooms, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants=  room.participants.all()
     
     
