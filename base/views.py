cb4621a52173b0865d73ca91c22f5a202bef8a48
imanirak <karinadgarces@gmail.com>
2022-08-16 22:15:34 -0400

    updated user creation form

--- base/views.py
+++ base/views.py
@@ -7,9 +7,9 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Room, Topic, Message, User
 from django.contrib.auth import authenticate, login, logout
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 from django.db.models import Q
-from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
@@ -50,10 +50,10 @@ def logoutUser(request):
 
 def registerPage(request):
     page = 'register'
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -190,7 +190,7 @@ def updateUser(request):
     form = UserForm(instance=user)
     
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
